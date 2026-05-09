02621ddc33166ffcbf1c9a3d167ff276348576a3
reygian61 <reygian61@MSI>
2026-05-09 15:28:40 +0800

    I added a eventlistener to the button for testing the javascript

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+function buttonClick() {
+    alert("The button is click")
+};
 
+const btn = document.getElementById("number-btn");
 
+btn.addEventListener("click", buttonClick, {once: false})
 
+console.log("HEllo world!");
